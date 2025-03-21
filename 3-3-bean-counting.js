2fb3edcb2d205136871f9ab55d895e19e8cd1c54
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-03-21 01:04:32 -0300

    3-3 bean counting

--- 3-3-bean-counting.js
+++ 3-3-bean-counting.js
@@ -1,6 +1,8 @@
 /* 
 You can get the Nth character, or letter, from a string by writing [N] after the string (for example, string[2]).
-The resulting value will be a string containing only one character (for example, "b"). The first character has position 0, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+The resulting value will be a string containing only one character (for example, "b"). 
+The first character has position 0, which causes the last one to be found at position string.length - 1. 
+In other words, a two-character string has length 2, and its characters have positions 0 and 1.
 
 Write a function called countBs that takes a string as its only argument and returns a number that indicates how 
 many uppercase B characters there are in the string.
@@ -10,3 +12,24 @@ except it takes a second argument that indicates the character that is to be cou
 (rather than counting only uppercase B characters). 
 Rewrite countBs to make use of this new function.
 */
+
+const countBs = (string) => {
+    let totalBs = 0;
+    for(var i = 0; i < string.length; i++ ) {
+        if(string[i] === "B") {
+            totalBs++
+        }
+    }
+    return totalBs;
+}
+
+const countChar = (string, letter) => {
+    let totalLetters = 0;
+    for(var i = 0; i < string.length; i++ ) {
+        if(string[i] === letter) {
+            totalLetters++
+        }
+    }
+    return totalLetters;
+
+}
