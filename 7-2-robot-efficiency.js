becdc3a1b76edcf5f3527e63bbe766869dac55bd
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-02 08:01:46 -0300

    chapter 7 exercises questions

---
+++ 7-2-robot-efficiency.js
@@ -0,0 +1,9 @@
+/* 
+7.2 Robot Efficiency
+
+Can you write a robot that finishes the delivery task faster than goalOrientedRobot? 
+If you observe that robot’s behavior, what obviously stupid things does it do? How could those be improved?
+
+If you solved the previous exercise, you might want to use your compareRobots function to verify whether 
+you improved the robot. 
+*/
