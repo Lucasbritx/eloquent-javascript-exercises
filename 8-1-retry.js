2e2df1452bc5e6d1567463d3f807eb2211ae8b21
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-04 08:32:39 -0300

    8-1 retry solution

--- 8-1-retry.js
+++ 8-1-retry.js
@@ -5,3 +5,25 @@ MultiplicatorUnitFailure. Write a function that wraps this clunky
 function and just keeps trying until a call succeeds, after which it returns the result.
 Make sure you handle only the exceptions you are trying to handle.
 */
+
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Multiplicator unit failure");
+  }
+}
+
+function CallPrimitiveMultiply() {
+  for (;;) {
+    try {
+      const result = primitiveMultiply(10, 5);
+      console.log(result);
+      return result;
+    } catch (error) {
+      if (error instanceof MultiplicatorUnitFailure) console.log(error);
+    }
+  }
+}
