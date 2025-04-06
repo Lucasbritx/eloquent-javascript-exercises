af13d4aa77061f029f9c76e97531ae9ff1fd90a4
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-06 08:29:41 -0300

    chapter 10 exercises questions

---
+++ 10-1-a-modular-robot.js
@@ -0,0 +1,25 @@
+/* 
+A package is a chunk of code that can be distributed on its own. NPM is a repository of JavaScript packages. 
+You can download all kinds of useful (and useless) packages from it.
+
+
+10.1 - A modular robot
+These are the bindings that the project from Chapter 7 creates:
+
+roads
+buildGraph
+roadGraph
+VillageState
+runRobot
+randomPick
+randomRobot
+mailRoute
+routeRobot
+findRoute
+goalOrientedRobot
+
+If you were to write that project as a modular program, what modules would you create? 
+Which module would depend on which other module, and what would their interfaces look like?
+
+Which pieces are likely to be available prewritten on NPM? Would you prefer to use an NPM package or write them yourself?
+*/
